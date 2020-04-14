965ae2d5448124e17e72e97a8193882cfbb0fab6
roxdavirox <roxdavirox@gmail.com>
2020-04-14 03:52:41 -0300

    refactor: comment unused

--- src/app/RootRoutes.jsx
+++ src/app/RootRoutes.jsx
@@ -2,27 +2,27 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 import dashboardRoutes from "./views/dashboard/DashboardRoutes";
-import utilitiesRoutes from "./views/utilities/UtilitiesRoutes";
 import sessionRoutes from "./views/sessions/SessionRoutes";
-
-import materialRoutes from "./views/material-kit/MaterialRoutes";
-import chartsRoute from "./views/charts/ChartsRoute";
-import dragAndDropRoute from "./views/Drag&Drop/DragAndDropRoute";
-import invoiceRoutes from "./views/invoice/InvoioceRoutes";
-import calendarRoutes from "./views/calendar/CalendarRoutes";
-import crudRoute from "./views/CRUD/CrudRoutes";
-import inboxRoute from "./views/inbox/InboxRoutes";
-import formsRoutes from "./views/forms/FormsRoutes";
-import mapRoutes from "./views/map/MapRoutes";
 import chatRoutes from "./views/chat-box/ChatRoutes";
-import todoRoutes from "./views/todo/TodoRoutes";
-import pageLayoutRoutes from "./views/page-layouts/PageLayoutRoutees";
-import ListRoute from "./views/list/ListRoute";
+
+// import utilitiesRoutes from "./views/utilities/UtilitiesRoutes";
+// import materialRoutes from "./views/material-kit/MaterialRoutes";
+// import chartsRoute from "./views/charts/ChartsRoute";
+// import dragAndDropRoute from "./views/Drag&Drop/DragAndDropRoute";
+// import invoiceRoutes from "./views/invoice/InvoioceRoutes";
+// import calendarRoutes from "./views/calendar/CalendarRoutes";
+// import crudRoute from "./views/CRUD/CrudRoutes";
+// import inboxRoute from "./views/inbox/InboxRoutes";
+// import formsRoutes from "./views/forms/FormsRoutes";
+// import mapRoutes from "./views/map/MapRoutes";
+// import todoRoutes from "./views/todo/TodoRoutes";
+// import pageLayoutRoutes from "./views/page-layouts/PageLayoutRoutees";
+// import ListRoute from "./views/list/ListRoute";
 
 
-import otherRoutes from "./views/others/OtherRoutes";
-import scrumBoardRoutes from "./views/scrum-board/ScrumBoardRoutes";
-import ecommerceRoutes from "./views/ecommerce/EcommerceRoutes";
+// import otherRoutes from "./views/others/OtherRoutes";
+// import scrumBoardRoutes from "./views/scrum-board/ScrumBoardRoutes";
+// import ecommerceRoutes from "./views/ecommerce/EcommerceRoutes";
 
 const redirectRoute = [
   {
