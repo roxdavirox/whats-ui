f503a9c963cc0e70444b8a1167fae5ddeb3bfe4d
roxdavirox <roxdavirox@gmail.com>
2020-05-06 06:34:00 -0300

    divider contact

--- src/app/views/chat-box/ContactList.jsx
+++ src/app/views/chat-box/ContactList.jsx
@@ -4,17 +4,21 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ChatAvatar from "./ChatAvatar";
+import Divider from '@material-ui/core/Divider';
 
 const ContactList = ({ contacts, handleContactClick }) => {
   return (
     <List>
       {Object.values(contacts) && Object.values(contacts).map((contact, index) => (
-        <ListItem button key={index}>
-          <ListItemIcon>
-          <ChatAvatar src={contact.eurl} />
-          </ListItemIcon>
-          <ListItemText primary={contact.name} onClick={() => handleContactClick(contact.jid)} />
-        </ListItem>
+        <>
+          <ListItem button key={index}>
+            <ListItemIcon>
+            <ChatAvatar src={contact.eurl} />
+            </ListItemIcon>
+            <ListItemText primary={contact.name} onClick={() => handleContactClick(contact.jid)} />
+          </ListItem>
+          <Divider />
+        </>
       ))}
     </List>
   );
