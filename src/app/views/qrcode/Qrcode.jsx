3f4a6569ba85076f7ae8864b93aa939021f8a9c6
roxdavirox <roxdavirox@gmail.com>
2020-05-09 05:02:37 -0300

    enviando dados login

--- src/app/views/qrcode/Qrcode.jsx
+++ src/app/views/qrcode/Qrcode.jsx
@@ -10,6 +10,11 @@ const QrcodeContainer = props => {
   const [rowsSelected, setRowsSelected] = useState([]);
 
 	useEffect(() => {
+    const connectedUser = {
+      userId: 'a069df2c-8abe-45a1-9e15-d5d3d62b5044',
+      ownerId: '8d4693dd-2fe3-41a5-913f-6e43118a70ee'
+    };
+    client.registerConnectHandler(() =>{}, connectedUser);
 		client.registerQrcodeHandler(handleQrcode);
 		client.registerContactsHandler(handleReceivedContacts);
 	}, []);
