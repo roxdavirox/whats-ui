863972ff1d58dfd22058ada96deaf970d7332277
roxdavirox <roxdavirox@gmail.com>
2020-05-28 06:54:36 -0300

    fix import names

--- src/app/RootRoutes.jsx
+++ src/app/RootRoutes.jsx
@@ -1,28 +1,10 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 
-import dashboardRoutes from "./views/dashboard/DashboardRoutes";
+import dashboardRoutes from "./pages/dashboard/DashboardRoutes";
 import sessionRoutes from "./pages/sessions/SessionRoutes";
-import chatRoutes from "./pages/chat-box/ChatRoutes";
+import chatRoutes from "./pages/chat/ChatRoutes";
 import qrcodeRoutes from './pages/qrcode/QrcodeRoutes';
-// import utilitiesRoutes from "./views/utilities/UtilitiesRoutes";
-// import materialRoutes from "./views/material-kit/MaterialRoutes";
-// import chartsRoute from "./views/charts/ChartsRoute";
-// import dragAndDropRoute from "./views/Drag&Drop/DragAndDropRoute";
-// import invoiceRoutes from "./views/invoice/InvoioceRoutes";
-// import calendarRoutes from "./views/calendar/CalendarRoutes";
-// import crudRoute from "./views/CRUD/CrudRoutes";
-// import inboxRoute from "./views/inbox/InboxRoutes";
-// import formsRoutes from "./views/forms/FormsRoutes";
-// import mapRoutes from "./views/map/MapRoutes";
-// import todoRoutes from "./views/todo/TodoRoutes";
-// import pageLayoutRoutes from "./views/page-layouts/PageLayoutRoutees";
-import ListRoute from "./views/list/ListRoute";
-
-
-// import otherRoutes from "./views/others/OtherRoutes";
-// import scrumBoardRoutes from "./views/scrum-board/ScrumBoardRoutes";
-// import ecommerceRoutes from "./views/ecommerce/EcommerceRoutes";
 
 const redirectRoute = [
   {
@@ -42,23 +24,7 @@ const routes = [
   ...sessionRoutes,
   ...dashboardRoutes,
   ...qrcodeRoutes,
-  // ...materialRoutes,
-  // ...utilitiesRoutes,
-  // ...chartsRoute,
-  // ...dragAndDropRoute,
-  // ...calendarRoutes,
-  // ...invoiceRoutes,
-  // ...crudRoute,
-  // ...inboxRoute,
-  // ...formsRoutes,
-  // ...mapRoutes,
   ...chatRoutes,
-  // ...todoRoutes,
-  // ...scrumBoardRoutes,
-  // ...ecommerceRoutes,
-  // ...pageLayoutRoutes,
-  // ...otherRoutes,
-  ...ListRoute,
   ...redirectRoute,
   ...errorRoute
 ];
