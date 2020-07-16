17f841f46edef50d9e0aab937402f9fdad5bf0d9
roxdavirox <roxdavirox@gmail.com>
2020-07-16 16:00:38 -0300

    remove selector from message item

--- src/app/pages/chat/MessageItem.jsx
+++ src/app/pages/chat/MessageItem.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import ChatAvatar from "./ChatAvatar";
 import shortid from "shortid";
 import Message from './Message';
-import { useSelector } from 'react-redux';
 
 const style = {
   messageItem: {
@@ -12,12 +10,6 @@ const style = {
 };
 
 const MessageItem = ({ message }) => {
-  const { 
-    contactId,
-    contacts,
-  } = useSelector(({ chat }) => chat);
-  const currentUser = useSelector(({ user }) => user);
-  const currentContact = contacts[contactId] || false ;
   const { key: { fromMe } } = message;
   return (
     <div 
@@ -25,16 +17,7 @@ const MessageItem = ({ message }) => {
       style={fromMe ? style.messageItem : {}}
       key={shortid.generate()}
     >
-      {/* <ChatAvatar
-        src={fromMe ? currentUser.eurl : currentContact.eurl}
-      /> */}
       <div className={fromMe ? 'mr-4' : 'ml-4' }>
-        {/* <p 
-          className="text-muted m-0 mb-2"
-          style={fromMe ? style.messageItem: {}}
-        >
-          {fromMe ? currentUser.name : currentContact.name}
-        </p> */}
         <div style={{ display: 'flex', justifyContent: `${fromMe ? 'flex-end' : 'flex-start' }` }}>
           <div
             className={`px-4 py-2 mb-2 list__message ${fromMe ? "bg-primary text-white" : "bg-paper"}`}
