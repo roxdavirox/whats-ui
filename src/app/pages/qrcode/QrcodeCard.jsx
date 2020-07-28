806dbd8f97bdd7a4d13934139bfce356db0d4580
roxdavirox <roxdavirox@gmail.com>
2020-07-28 17:43:42 -0300

    cleaar qrcode card

--- src/app/pages/qrcode/QrcodeCard.jsx
+++ src/app/pages/qrcode/QrcodeCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Card, Grid, Button } from "@material-ui/core";
-import QrcodeVideo from './QrcodeVideo';
 import Qrcode from 'qrcode.react';
 import Skeleton from 'react-loading-skeleton';
 
@@ -8,23 +7,7 @@ const QrcodeCard = ({ qrcode, isConnected }) => (
   <div className="pricing m-sm-30 position-relative">
     <div>
       <Grid container spacing={4} style={{ justifyContent: 'space-evenly' }}>
-          {/* <QrcodeVideo startVideo/> */}
-        <Grid item lg={4} md={6} sm={6} xs={12}>
-          <div className="w-full text-left ml-1 mb-11">
-            <h3 className="m-0 pl-5 pt-4 pb-2 font-medium">
-              Para usar o whatspipe no seu computador:
-            </h3>
-            <p className="m-0 pl-10 pt-6 text-muted">
-              1. Abra o Whatsapp
-            </p>
-            <p className="m-0 pl-10 pt-6 text-muted">
-              2. Toque em mais <b>Opções</b> ou <b>Ajustes</b> e selecione <b>Whatsapp Web</b>
-            </p>
-            <p className="m-0 pl-10 pt-6 text-muted">
-              3. Aponte seu celular para essa tela para capturar o código
-            </p>
-          </div>
-        </Grid>
+
         <Grid item lg={4} md={6} sm={6} xs={12}>
           <Card elevation={6} className="pricing__card text-center p-sm-24">
             <div className="mb-4">
